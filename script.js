c2a2f35eedb0f08e06b21817ab67f85a98de938e
kvniranwb <niranjanvt967@gmail.com>
2026-05-10 15:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ window.addEventListener("load", () => {
 });
 
 inputs.forEach((input, index) => {
-  input.addEventListener("input", (e) => {
+
+  input.addEventListener("input", () => {
     // allow only digits
     input.value = input.value.replace(/[^0-9]/g, "");
 
-    // move to next input if value exists
+    // move forward
     if (input.value && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
+
     if (e.key === "Backspace") {
+
       if (input.value === "") {
-        // move to previous input if exists
         if (index > 0) {
           inputs[index - 1].focus();
           inputs[index - 1].value = "";
@@ -27,4 +29,5 @@ inputs.forEach((input, index) => {
       }
     }
   });
+
 });
